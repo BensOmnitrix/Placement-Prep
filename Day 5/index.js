ec6b8a1eeac22110861b76d369cd401bfd138470
BensOmnitrix <lakshaynandu.batraben10@gmail.com>
2025-08-07 23:43:16 +0530

    Express JS Important Things

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+// const port = 3000;
+const port = process.env.PORT || 3000;
+
 const app = express();
-const port = 3000;
+const path = require('path');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
     res.send("Hello World");
 })
 
-app.listen(port,function(){
-    console.log(`The server is running on ${port}`);
+app.post('/post',function(req,res){
+    console.log(req.headers['authorization']);
+    console.log(req.body)
+    console.log(req.query.message);
+    res.send({
+        msg: '2+2 = 4' 
+    })
 });
+
+app.get('/route-handler',function(req,res){
+    //req consists of headers,body, query parameters
+    res.json({
+        name: "Harkirat",
+        age: 21
+    })
+})
+
+app.get('/conversations',function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.get("/try",function(req,res){
+    res.send("<b>Hi bro</b>")
+})
+
+app.listen(port,function(){
+    console.log("The server is successfully running on the port number " + port);
+})
