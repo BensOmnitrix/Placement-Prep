69b41df9ceb5f6642526a5d6e635f3ae3f259534
BensOmnitrix <lakshaynandu.batraben10@gmail.com>
2025-08-28 11:09:36 +0530

    Minor Update

--- BensOmnitrix DEV/Day 20/components/Todo.jsx
+++ BensOmnitrix DEV/Day 20/components/Todo.jsx
@@ -1,25 +1,49 @@
 function Todo(props) {
-    function deleteHandler(indx){
-        const updatedTodo = props.allTodos.filter((todo) => todo.id != indx);
-        return updatedTodo;
-    }
+  function deleteHandler(id) {
+    const updatedTodo = props.allTodos.filter((todo) => todo.id != id);
+    props.setTodo(updatedTodo);
+  }
 
-    return (
-        <>
-            <div className="todo">
-                <div className="title">Title: {props.todo.title}</div>
-                <div className="description">Description: {props.todo.description}</div>
-                <br />
-                <div>
-                    <span className="completed"><button>{props.todo.completed ? "Done!" : "Mark as Done"}</button></span>
-                    <span className="delete"><button onClick={() => {
-                        props.setTodo(deleteHandler(props.todo.id));
-                    }}>Delete</button></span>
-                </div>
-                <br />
-            </div>
-        </>
-    )
+  function DoneHandler(id) {
+    const updatedTodo = props.allTodos.map((todo) => {
+      if (todo.id == id) {
+        return { ...props.todo, completed: !props.todo.completed };
+      }
+      return props.todo;
+    });
+    props.setTodo(updatedTodo);
+  }
+
+  return (
+    <>
+      <div className="todo">
+        <div className="title">Title: {props.todo.title}</div>
+        <div className="description">Description: {props.todo.description}</div>
+        <br />
+        <div>
+          <span className="completed">
+            <button
+              onClick={() => {
+                DoneHandler(props.todo.id);
+              }}
+            >
+              {props.todo.completed ? "Done!" : "Mark as Done"}
+            </button>
+          </span>
+          <span className="delete">
+            <button
+              onClick={() => {
+                deleteHandler(props.todo.id);
+              }}
+            >
+              Delete
+            </button>
+          </span>
+        </div>
+        <br />
+      </div>
+    </>
+  );
 }
 
-export default Todo
+export default Todo;
